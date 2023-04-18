91e5db716560b34ede36f7fb614f2b168d37c6a4
glen coden <info@simonmeyer.de>
2023-04-18 18:16:22 +0200

    layout for postgres db connection via glencoden cloud

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
     // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
     // MY_KV_NAMESPACE: KVNamespace;
@@ -20,7 +10,7 @@ export interface Env {
     //
     // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
     // MY_SERVICE: Fetcher;
-    //
+
     // Deploy database with the following three steps:
     //
     // wrangler d1 execute <DATABASE_NAME> --file=./schema.sql
@@ -30,7 +20,7 @@ export interface Env {
     // Client API: https://developers.cloudflare.com/d1/platform/client-api/
     //
     // Commands: https://developers.cloudflare.com/workers/wrangler/commands/#d1
-    //
+
     DB: D1Database;
 }
 
@@ -50,6 +40,10 @@ export default {
                 .all()
 
             return Response.json(results)
+        }
+
+        if (pathname === 'pg') {
+            // implement request to glencoden cloud
         }
 
         return new Response('Hello from the Square Cards API!')
